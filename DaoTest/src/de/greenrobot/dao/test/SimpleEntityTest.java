86cfd1fbcb6783b6aaac656d315d92fa39912388
Markus Junginger <markus@greenrobot>
2011-08-01 19:35:46 +0200

    nullable update tests

--- DaoTest/src/de/greenrobot/dao/test/SimpleEntityTest.java
+++ DaoTest/src/de/greenrobot/dao/test/SimpleEntityTest.java
@@ -26,6 +26,82 @@ public class SimpleEntityTest extends AbstractDaoTestLongPk<SimpleEntityDao, Sim
         assertNotSame(entity, reloaded);
 
         assertEquals(entity.getId(), reloaded.getId());
+        assertValuesNull(reloaded);
+    }
+
+    public void testValues() {
+        SimpleEntity entity = createEntity(1l);
+        setValues(entity);
+        dao.insert(entity);
+        SimpleEntity reloaded = dao.load(1l);
+        assertNotSame(entity, reloaded);
+        assertValues(reloaded);
+    }
+
+    public void testUpdateValues() {
+        SimpleEntity entity = createEntity(1l);
+        dao.insert(entity);
+        entity = dao.load(1l);
+        setValues(entity);
+        dao.update(entity);
+        SimpleEntity reloaded = dao.load(1l);
+        assertNotSame(entity, reloaded);
+        assertValues(reloaded);
+    }
+
+    public void testUpdateValuesToNull() {
+        SimpleEntity entity = createEntity(1l);
+        setValues(entity);
+        dao.insert(entity);
+        entity = dao.load(1l);
+        assertValues(entity);
+        setValuesToNull(entity);
+        dao.update(entity);
+        SimpleEntity reloaded = dao.load(1l);
+        assertNotSame(entity, reloaded);
+        assertValuesNull(reloaded);
+    }
+
+    protected void assertValues(SimpleEntity reloaded) {
+        assertEquals(1l, (long) reloaded.getId());
+        assertEquals(true, (boolean) reloaded.getSimpleBoolean());
+        assertEquals(Double.MAX_VALUE, reloaded.getSimpleDouble());
+        assertEquals(Float.MAX_VALUE, reloaded.getSimpleFloat());
+        assertEquals(Long.MAX_VALUE, (long) reloaded.getSimpleLong());
+        assertEquals(Byte.MAX_VALUE, (byte) reloaded.getSimpleByte());
+        assertEquals(Integer.MAX_VALUE, (int) reloaded.getSimpleInt());
+        assertEquals(Short.MAX_VALUE, (short) reloaded.getSimpleShort());
+        assertEquals("greenrobot greenDAO", reloaded.getSimpleString());
+        byte[] bytes = { 42, -17, 23, 0, 127, -128 };
+        assertTrue(Arrays.equals(bytes, reloaded.getSimpleByteArray()));
+    }
+
+    protected void setValues(SimpleEntity entity) {
+        entity.setSimpleBoolean(true);
+        entity.setSimpleByte(Byte.MAX_VALUE);
+        entity.setSimpleShort(Short.MAX_VALUE);
+        entity.setSimpleInt(Integer.MAX_VALUE);
+        entity.setSimpleLong(Long.MAX_VALUE);
+        entity.setSimpleFloat(Float.MAX_VALUE);
+        entity.setSimpleDouble(Double.MAX_VALUE);
+        entity.setSimpleString("greenrobot greenDAO");
+        byte[] bytes = { 42, -17, 23, 0, 127, -128 };
+        entity.setSimpleByteArray(bytes);
+    }
+
+    protected void setValuesToNull(SimpleEntity entity) {
+        entity.setSimpleBoolean(null);
+        entity.setSimpleByte(null);
+        entity.setSimpleShort(null);
+        entity.setSimpleInt(null);
+        entity.setSimpleLong(null);
+        entity.setSimpleFloat(null);
+        entity.setSimpleDouble(null);
+        entity.setSimpleString(null);
+        entity.setSimpleByteArray(null);
+    }
+
+    protected void assertValuesNull(SimpleEntity reloaded) {
         assertNull(reloaded.getSimpleBoolean());
         assertNull(reloaded.getSimpleDouble());
         assertNull(reloaded.getSimpleFloat());
@@ -36,37 +112,6 @@ public class SimpleEntityTest extends AbstractDaoTestLongPk<SimpleEntityDao, Sim
         assertNull(reloaded.getSimpleBoolean());
         assertNull(reloaded.getSimpleString());
         assertNull(reloaded.getSimpleByteArray());
-    }
-
-    public void testValues() {
-        SimpleEntity entity = createEntity(1l);
-
-        entity.setSimpleBoolean(true);
-        entity.setSimpleByte    (Byte.MAX_VALUE);
-        entity.setSimpleShort   (Short.MAX_VALUE);
-        entity.setSimpleInt     (Integer.MAX_VALUE);
-        entity.setSimpleLong    (Long.MAX_VALUE);
-        entity.setSimpleFloat   (Float.MAX_VALUE);
-        entity.setSimpleDouble  (Double.MAX_VALUE);
-        entity.setSimpleString("greenrobot greenDAO");
-        byte[] bytes = { 42, -17, 23, 0, 127, -128 };
-        entity.setSimpleByteArray(bytes);
-
-        dao.insert(entity);
-
-        SimpleEntity reloaded = dao.load(1l);
-        assertNotSame(entity, reloaded);
-
-        assertEquals(1l, (long) reloaded.getId());
-        assertEquals(true, (boolean)reloaded.getSimpleBoolean());
-        assertEquals(Double.MAX_VALUE, reloaded.getSimpleDouble());
-        assertEquals(Float.MAX_VALUE, reloaded.getSimpleFloat());
-        assertEquals(Long.MAX_VALUE, (long) reloaded.getSimpleLong());
-        assertEquals(Byte.MAX_VALUE,  (byte) reloaded.getSimpleByte());
-        assertEquals(Integer.MAX_VALUE, (int) reloaded.getSimpleInt());
-        assertEquals(Short.MAX_VALUE, (short) reloaded.getSimpleShort());
-        assertEquals("greenrobot greenDAO",  reloaded.getSimpleString());
-        assertTrue(Arrays.equals(bytes, reloaded.getSimpleByteArray()));
     }
 
 }
