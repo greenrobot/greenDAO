aa1cf1a787119fd3cea442b9734c30441cdd4a70
Markus Junginger <markus@greenrobot>
2011-08-16 11:00:10 +0200

    testNotEqString

--- DaoTest/src/de/greenrobot/dao/test/QueryBuilderSimpleTest.java
+++ DaoTest/src/de/greenrobot/dao/test/QueryBuilderSimpleTest.java
@@ -10,13 +10,53 @@ public class QueryBuilderSimpleTest extends TestEntityTestBase {
     public void testEqInteger() {
         ArrayList<TestEntity> inserted = insert(3);
         int value = getSimpleInteger(1);
-        
+
         List<TestEntity> result = dao.queryBuilder().eq(Properties.SimpleInteger, value).build().list();
         assertEquals(1, result.size());
-        
+
         TestEntity resultEntity = result.get(0);
         assertEquals(value, (int) resultEntity.getSimpleInteger());
         assertEquals(inserted.get(1).getId(), resultEntity.getId());
+    }
+
+    public void testEqString() {
+        ArrayList<TestEntity> inserted = insert(3);
+        String value = getSimpleString(1);
+
+        List<TestEntity> result = dao.queryBuilder().eq(Properties.SimpleString, value).build().list();
+        assertEquals(1, result.size());
+
+        TestEntity resultEntity = result.get(0);
+        assertEquals(value, resultEntity.getSimpleString());
+        assertEquals(inserted.get(1).getId(), resultEntity.getId());
+    }
+
+    public void testEqStringAndInteger() {
+        ArrayList<TestEntity> inserted = insert(3);
+        String valueStr = getSimpleString(1);
+        int valueInt = getSimpleInteger(1);
+
+        List<TestEntity> result = dao.queryBuilder().eq(Properties.SimpleString, valueStr)
+                .eq(Properties.SimpleInteger, valueInt).build().list();
+        assertEquals(1, result.size());
+
+        TestEntity resultEntity = result.get(0);
+        assertEquals(inserted.get(1).getId(), resultEntity.getId());
+    }
+
+    public void testNotEqString() {
+        ArrayList<TestEntity> inserted = insert(3);
+        String value = getSimpleString(1);
+
+        List<TestEntity> result = dao.queryBuilder().notEq(Properties.SimpleString, value).build().list();
+        assertEquals(2, result.size());
+
+        TestEntity resultEntity1 = result.get(0);
+        TestEntity resultEntity2 = result.get(1);
+        long loId = Math.min(resultEntity1.getId(), resultEntity2.getId());
+        long hiId = Math.max(resultEntity1.getId(), resultEntity2.getId());
+        assertEquals((long) inserted.get(0).getId(), loId);
+        assertEquals((long) inserted.get(2).getId(), hiId);
     }
 
 }
