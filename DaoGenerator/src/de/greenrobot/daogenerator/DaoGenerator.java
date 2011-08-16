7b5f439483250b42657c7b37a9b07306280495b8
Markus Junginger <markus@greenrobot>
2011-08-16 16:36:38 +0200

    added GPL header

--- DaoGenerator/src/de/greenrobot/daogenerator/DaoGenerator.java
+++ DaoGenerator/src/de/greenrobot/daogenerator/DaoGenerator.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2011 Markus Junginger, greenrobot (http://greenrobot.de)
+ *
+ * This file is part of greenDAO Generator.
+ * 
+ * greenDAO Generator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * greenDAO Generator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with greenDAO Generator.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.greenrobot.daogenerator;
 
 import java.io.File;
@@ -30,6 +47,7 @@ public class DaoGenerator {
 
         System.out.println("greenDAO Generator (preview)");
         System.out.println("Copyright 2011 Markus Junginger, greenrobot.de. Licensed under GPL V3.");
+        System.out.println("This program comes with ABSOLUTELY NO WARRANTY");
 
         File outDirFile = toFileForceExists(outDir);
 
