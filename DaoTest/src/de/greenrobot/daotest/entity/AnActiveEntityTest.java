297fecc85df7f558332d36bc9568a9fa32b2aa15
Markus Junginger <markus@greenrobot>
2011-10-23 09:36:35 +0200

    added missing license header to test classes

--- DaoTest/src/de/greenrobot/daotest/entity/AnActiveEntityTest.java
+++ DaoTest/src/de/greenrobot/daotest/entity/AnActiveEntityTest.java
@@ -1,14 +1,28 @@
+/*
+ * Copyright (C) 2011 Markus Junginger, greenrobot (http://greenrobot.de)
+ *
+ * This file is part of greenDAO Generator.
+ * 
+ * greenDAO Generator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * greenDAO Generator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with greenDAO Generator.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.greenrobot.daotest.entity;
 
 import de.greenrobot.dao.DaoException;
 import de.greenrobot.dao.test.AbstractDaoSessionTest;
-import de.greenrobot.dao.test.AbstractDaoTestLongPk;
 import de.greenrobot.daotest.AnActiveEntity;
 import de.greenrobot.daotest.AnActiveEntityDao;
 import de.greenrobot.daotest.DaoMaster;
 import de.greenrobot.daotest.DaoSession;
-import de.greenrobot.daotest.ToManyEntityDao;
-import de.greenrobot.daotest.ToManyTargetEntityDao;
 
 public class AnActiveEntityTest extends  AbstractDaoSessionTest<DaoMaster, DaoSession> {
 
