bb522d9719aad055bd78e81b6aaf6f142eb44fd5
Markus Junginger <markus@greenrobot>
2012-06-10 10:33:39 +0200

    fixed createEntityWithRandomPk: create collision free PKs

--- DaoCore/src/de/greenrobot/dao/test/AbstractDaoTestSinglePk.java
+++ DaoCore/src/de/greenrobot/dao/test/AbstractDaoTestSinglePk.java
@@ -288,6 +288,7 @@ public abstract class AbstractDaoTestSinglePk<D extends AbstractDao<T, K>, T, K>
         return cursor;
     }
 
+    /** Provides a collision free PK () not returned before in the current test. */
     protected K nextPk() {
         for (int i = 0; i < 100000; i++) {
             K pk = createRandomPk();
@@ -299,7 +300,7 @@ public abstract class AbstractDaoTestSinglePk<D extends AbstractDao<T, K>, T, K>
     }
 
     protected T createEntityWithRandomPk() {
-        return createEntity(createRandomPk());
+        return createEntity(nextPk());
     }
 
     /** K does not have to be collision free, check nextPk for collision free PKs. */
