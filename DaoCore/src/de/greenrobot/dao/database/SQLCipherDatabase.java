f7c78eb1ba3d4a56f5119686440da9427fe8fee7
Markus <markus@greenrobot>
2015-08-13 20:11:24 +0200

    SQLCipher database implementation

---
+++ DaoCore/src/de/greenrobot/dao/database/SQLCipherDatabase.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2011-2015 Markus Junginger, greenrobot (http://greenrobot.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.greenrobot.dao.database;
+
+import android.database.Cursor;
+import android.database.SQLException;
+import net.sqlcipher.database.SQLiteDatabase;
+
+public class SQLCipherDatabase implements Database {
+    private final SQLiteDatabase delegate;
+
+    public SQLCipherDatabase(SQLiteDatabase delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public Cursor rawQuery(String sql, String[] selectionArgs) {
+        return delegate.rawQuery(sql, selectionArgs);
+    }
+
+    @Override
+    public void execSQL(String sql) throws SQLException {
+        delegate.execSQL(sql);
+    }
+
+    @Override
+    public void beginTransaction() {
+        delegate.beginTransaction();
+    }
+
+    @Override
+    public void endTransaction() {
+        delegate.endTransaction();
+    }
+
+    @Override
+    public boolean inTransaction() {
+        return delegate.inTransaction();
+    }
+
+    @Override
+    public void setTransactionSuccessful() {
+        delegate.setTransactionSuccessful();
+    }
+
+    @Override
+    public void execSQL(String sql, Object[] bindArgs) throws SQLException {
+        delegate.execSQL(sql, bindArgs);
+    }
+
+    @Override
+    public DatabaseStatement compileStatement(String sql) {
+        return new SQLCipherStatement(delegate.compileStatement(sql));
+    }
+
+    @Override
+    public boolean isDbLockedByCurrentThread() {
+        return delegate.isDbLockedByCurrentThread();
+    }
+
+    @Override
+    public void close() {
+        delegate.close();
+    }
+
+    public SQLiteDatabase getSQLiteDatabase() {
+        return delegate;
+    }
+}
