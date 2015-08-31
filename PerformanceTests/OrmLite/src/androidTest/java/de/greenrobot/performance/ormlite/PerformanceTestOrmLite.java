3da7fd20b1e1c940ef567b0732fd0f7fa97e9694
greenrobot Team <opensource+team@greenrobot>
2015-08-31 10:50:40 +0200

    Move perf tests to their own directory.
    - Also clean up OrmLite test, restructure to recommended gradle folder hierarchy.

--- PerformanceTests/OrmLite/src/androidTest/java/de/greenrobot/performance/ormlite/PerformanceTestOrmLite.java
+++ PerformanceTests/OrmLite/src/androidTest/java/de/greenrobot/performance/ormlite/PerformanceTestOrmLite.java
@@ -1,5 +1,6 @@
 package de.greenrobot.performance.ormlite;
 
+import de.greenrobot.performance.ormlite.BuildConfig;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
