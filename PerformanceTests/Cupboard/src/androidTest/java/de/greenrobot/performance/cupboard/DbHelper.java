f90e0b707d3307b703050b8bbeccc21157e4a274
Eric Kok <erickok@gmail.com>
2015-09-10 17:10:23 +0200

    Comparison performance test for the Cupboard library.

---
+++ PerformanceTests/Cupboard/src/androidTest/java/de/greenrobot/performance/cupboard/DbHelper.java
@@ -0,0 +1,32 @@
+package de.greenrobot.performance.cupboard;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+	static {
+		// register our models
+		cupboard().register(SimpleEntityNotNull.class);
+	}
+
+	public DbHelper(Context context, String databaseName, int databaseVersion) {
+		super(context, databaseName, null, databaseVersion);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// this will ensure that all tables are created
+		cupboard().withDatabase(db).createTables();
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// this will upgrade tables, adding columns and new tables.
+		// Note that existing columns will not be converted
+		cupboard().withDatabase(db).upgradeTables();
+	}
+}
