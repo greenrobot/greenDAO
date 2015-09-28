9ef2f14d81efca0eab0de20172f5ed0ebf8f6101
greenrobot Team <opensource+team@greenrobot>
2015-09-28 09:04:49 +0200

    Add perf test for raw SQLite.

---
+++ PerformanceTests/Sqlite/src/androidTest/java/de/greenrobot/performance/sqlite/IndexedStringEntity.java
@@ -0,0 +1,12 @@
+package de.greenrobot.performance.sqlite;
+
+/**
+ * Simple entity with a string property that is indexed.
+ */
+public class IndexedStringEntity {
+
+    public Long _id;
+
+    public String indexedString;
+
+}
