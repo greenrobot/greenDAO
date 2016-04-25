af59c986223df02dbcbafffe4277417a63ac72a4
greenrobot Team <team@greenrobot>
2016-04-25 17:13:39 +0200

    Support @ToOne without foreign key

--- DaoCore/src/main/java/org/greenrobot/greendao/annotations/ToOne.java
+++ DaoCore/src/main/java/org/greenrobot/greendao/annotations/ToOne.java
@@ -11,6 +11,20 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.FIELD)
 public @interface ToOne {
-    /** Name of the property which holds the key */
-    String foreignKey();
+    /**
+     * Name of the property inside the current entity which holds the key of related entity
+     * You should specify either foreignKey or {@link #mappedBy}
+     */
+    String foreignKey() default "";
+
+    /**
+     * Name of the property inside the target entity which holds id of the source (current) entity
+     * You should specify either {@link #foreignKey} or mappedBy
+     */
+    String mappedBy() default "";
+
+    /**
+     * Whether the relation is unique. Applicable only if {@link #mappedBy} is in use
+     */
+    boolean unique() default false;
 }
