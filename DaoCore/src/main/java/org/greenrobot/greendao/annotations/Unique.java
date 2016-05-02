44a3aee5af0d4cb3da871542c9de07ac53816912
greenrobot Team <team@greenrobot>
2016-05-02 22:45:31 +0200

    add @Unique annotation

---
+++ DaoCore/src/main/java/org/greenrobot/greendao/annotations/Unique.java
@@ -0,0 +1,25 @@
+package org.greenrobot.greendao.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks property should have a UNIQUE constraint during table creation.
+ *
+ * <p>
+ * To have a unique constraint after table creation you can use {@link Index#unique()}
+ * </p>
+ *
+ * <p>
+ * Note having both @Unique and {@link Index} is redundant and causes performance decrease
+ * on DB level. See <a href="https://www.sqlite.org/lang_createtable.html">here</a> for more information.
+ * </p>
+ *
+ * @see Index#unique()
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.FIELD)
+public @interface Unique {
+}
