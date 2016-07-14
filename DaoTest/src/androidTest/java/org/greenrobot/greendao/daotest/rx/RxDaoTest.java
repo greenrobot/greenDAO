6f55fe71f1e7471723d8a10a17cd542ab943776c
Markus <markus@greenrobot>
2016-07-14 21:16:27 +0200

    test(No)Scheduler

--- DaoTest/src/androidTest/java/org/greenrobot/greendao/daotest/rx/RxDaoTest.java
+++ DaoTest/src/androidTest/java/org/greenrobot/greendao/daotest/rx/RxDaoTest.java
@@ -1,8 +1,8 @@
 package org.greenrobot.greendao.daotest.rx;
 
-import org.greenrobot.greendao.rx.RxDao;
 import org.greenrobot.greendao.daotest.TestEntity;
 import org.greenrobot.greendao.daotest.TestEntityDao;
+import org.greenrobot.greendao.rx.RxDao;
 import org.greenrobot.greendao.test.AbstractDaoTest;
 
 import java.util.List;
@@ -25,20 +25,24 @@ public class RxDaoTest extends AbstractDaoTest<TestEntityDao, TestEntity, Long> 
         rxDao = dao.rx();
     }
 
+    public void testScheduler() {
+        TestSubscriber<List<TestEntity>> testSubscriber = awaitTestSubscriber(rxDao.loadAll());
+        Thread lastSeenThread = testSubscriber.getLastSeenThread();
+        assertNotSame(lastSeenThread, Thread.currentThread());
+    }
+
+    public void testNoScheduler() {
+        RxDao<TestEntity, Long> rxDaoNoScheduler = new RxDao<>(dao);
+        TestSubscriber<List<TestEntity>> testSubscriber = awaitTestSubscriber(rxDaoNoScheduler.loadAll());
+        Thread lastSeenThread = testSubscriber.getLastSeenThread();
+        assertSame(lastSeenThread, Thread.currentThread());
+    }
+
     public void testLoadAll() {
         insertEntity("foo");
         insertEntity("bar");
 
-        Observable<List<TestEntity>> observable = rxDao.loadAll();
-        TestSubscriber<List<TestEntity>> testSubscriber = new TestSubscriber<>();
-        observable.subscribe(testSubscriber);
-        testSubscriber.awaitTerminalEvent(3, TimeUnit.SECONDS);
-        testSubscriber.assertCompleted();
-        testSubscriber.assertNoErrors();
-
-        Thread lastSeenThread = testSubscriber.getLastSeenThread();
-        assertNotSame(lastSeenThread, Thread.currentThread());
-
+        TestSubscriber<List<TestEntity>> testSubscriber = awaitTestSubscriber(rxDao.loadAll());
         assertEquals(1, testSubscriber.getValueCount());
         List<TestEntity> entities = testSubscriber.getOnNextEvents().get(0);
 
@@ -56,6 +60,15 @@ public class RxDaoTest extends AbstractDaoTest<TestEntityDao, TestEntity, Long> 
         }
         assertEquals(1, foo);
         assertEquals(1, bar);
+    }
+
+    private TestSubscriber<List<TestEntity>> awaitTestSubscriber(Observable<List<TestEntity>> observable) {
+        TestSubscriber<List<TestEntity>> testSubscriber = new TestSubscriber<>();
+        observable.subscribe(testSubscriber);
+        testSubscriber.awaitTerminalEvent(3, TimeUnit.SECONDS);
+        testSubscriber.assertCompleted();
+        testSubscriber.assertNoErrors();
+        return testSubscriber;
     }
 
     protected TestEntity insertEntity(String simpleStringNotNull) {
