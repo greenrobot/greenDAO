2147b2d477c7881739744783784ccfd5ee2c4905
Markus <markus@greenrobot>
2016-07-14 21:06:31 +0200

    emulate Observable.fromCallable while still in beta, 2nd try

--- DaoCore/src/main/java/org/greenrobot/greendao/rx/RxUtils.java
+++ DaoCore/src/main/java/org/greenrobot/greendao/rx/RxUtils.java
@@ -3,10 +3,23 @@ package org.greenrobot.greendao.rx;
 import java.util.concurrent.Callable;
 
 import rx.Observable;
+import rx.functions.Func0;
 
 class RxUtils {
     /** As of RxJava 1.1.7, Observable.fromCallable is still @Beta, so just in case... */
-    static <T> Observable<T> fromCallable(Callable<? extends T> callable) {
-        return Observable.create(new OnSubscribeFromCallable<T>(callable));
+    static <T> Observable<T> fromCallable(final Callable<T> callable) {
+        return Observable.defer(new Func0<Observable<T>>() {
+
+            @Override
+            public Observable<T> call() {
+                T result;
+                try {
+                    result = callable.call();
+                } catch (Exception e) {
+                    return Observable.error(e);
+                }
+                return Observable.just(result);
+            }
+        });
     }
 }
