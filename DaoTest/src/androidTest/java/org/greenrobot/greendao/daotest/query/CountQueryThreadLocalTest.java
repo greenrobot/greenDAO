a56610d2760a353bcd068dbc5fc9e449b5747264
Markus <markus@greenrobot>
2016-08-01 11:39:52 +0200

    updated/unified copyright texts

--- DaoTest/src/androidTest/java/org/greenrobot/greendao/daotest/query/CountQueryThreadLocalTest.java
+++ DaoTest/src/androidTest/java/org/greenrobot/greendao/daotest/query/CountQueryThreadLocalTest.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2011-2013 Markus Junginger, greenrobot (http://greenrobot.de)
+ * Copyright (C) 2011-2016 Markus Junginger, greenrobot (http://greenrobot.org)
  *
  * This file is part of greenDAO Generator.
- * 
+ *
  * greenDAO Generator is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -11,10 +11,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with greenDAO Generator.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.greenrobot.greendao.daotest.query;
 
 import org.greenrobot.greendao.DaoException;
