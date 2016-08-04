greenrobot/greenDAO

commit 1fd5a337edb91ad53439ab9828f71d7e7df63a59

Process.myTid() seems to have issues on some devices (see Github #376)

DaoCore/src/main/java/org/greenrobot/greendao/query/AbstractQueryData.java
@@ -16,24 +16,25 @@
 
 package org.greenrobot.greendao.query;
 
-import java.lang.ref.WeakReference;
-
-import android.os.Process;
-import android.util.SparseArray;
-
 import org.greenrobot.greendao.AbstractDao;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 abstract class AbstractQueryData<T, Q extends AbstractQuery<T>> {
     final String sql;
     final AbstractDao<T, ?> dao;
     final String[] initialValues;
-    final SparseArray<WeakReference<Q>> queriesForThreads;
+    final Map<Long, WeakReference<Q>> queriesForThreads;
 
     AbstractQueryData(AbstractDao<T, ?> dao, String sql, String[] initialValues) {
         this.dao = dao;
         this.sql = sql;
         this.initialValues = initialValues;
-        queriesForThreads = new SparseArray<WeakReference<Q>>();
+        queriesForThreads = new HashMap<>();
     }
 
     /**
@@ -53,15 +54,10 @@ abstract class AbstractQueryData<T, Q extends AbstractQuery<T>> {
      * Note: all parameters are reset to their initial values specified in {@link QueryBuilder}.
      */
     Q forCurrentThread() {
-        int threadId = Process.myTid();
-        if (threadId == 0) {
-            // Workaround for Robolectric, always returns 0
-            long id = Thread.currentThread().getId();
-            if (id < 0 || id > Integer.MAX_VALUE) {
-                throw new RuntimeException("Cannot handle thread ID: " + id);
-            }
-            threadId = (int) id;
-        }
+        // Process.myTid() seems to have issues on some devices (see Github #376) and Robolectric (#171):
+        // We use currentThread().getId() instead (unfortunately return a long, can not use SparseArray).
+        // PS.: thread ID may be reused, which should be fine because old thread will be gone anyway.
+        long threadId = Thread.currentThread().getId();
         synchronized (queriesForThreads) {
             WeakReference<Q> queryRef = queriesForThreads.get(threadId);
             Q query = queryRef != null ? queryRef.get() : null;
@@ -80,9 +76,11 @@ abstract class AbstractQueryData<T, Q extends AbstractQuery<T>> {
 
     void gc() {
         synchronized (queriesForThreads) {
-            for (int i = queriesForThreads.size() - 1; i >= 0; i--) {
-                if (queriesForThreads.valueAt(i).get() == null) {
-                    queriesForThreads.remove(queriesForThreads.keyAt(i));
+            Iterator<Entry<Long, WeakReference<Q>>> iterator = queriesForThreads.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Entry<Long, WeakReference<Q>> entry = iterator.next();
+                if (entry.getValue() == null) {
+                    iterator.remove();
                 }
             }
         }

commit 13353325ee741ed402629047759f0f891628571a

fix leak introduced 2 days in 1fd5a337edb91ad53439ab9828f71d7e7df63a59

DaoCore/src/main/java/org/greenrobot/greendao/query/AbstractQueryData.java
@@ -79,7 +79,7 @@ abstract class AbstractQueryData<T, Q extends AbstractQuery<T>> {
             Iterator<Entry<Long, WeakReference<Q>>> iterator = queriesForThreads.entrySet().iterator();
             while (iterator.hasNext()) {
                 Entry<Long, WeakReference<Q>> entry = iterator.next();
-                if (entry.getValue() == null) {
+                if (entry.getValue().get() == null) {
                     iterator.remove();
                 }
             }