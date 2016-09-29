4dea8309a020b93bcdf1108e2a07598d6cfd251e
3pmTea <3pmTea@gmail.com>
2016-09-29 11:14:51 +0800

    Update QueryBuilder.java

--- DaoCore/src/main/java/org/greenrobot/greendao/query/QueryBuilder.java
+++ DaoCore/src/main/java/org/greenrobot/greendao/query/QueryBuilder.java
@@ -269,7 +269,7 @@ public class QueryBuilder<T> {
     }
 
     /**
-     * Sets the offset for query results in combination with {@link #limit(int)}. The first {@code limit} results are
+     * Sets the offset for query results in combination with {@link #limit(int)}. The first {@code offset} results are
      * skipped and the total number of results will be limited by {@code limit}. You cannot use offset without limit.
      */
     public QueryBuilder<T> offset(int offset) {
